86fdefd910155a9c7d193bdb77ba6691b73d49a3
idabkows <01169158@pw.edu.pl>
2022-06-04 14:19:43 +0200

    Update main.cpp

--- ChessApp/main.cpp
+++ ChessApp/main.cpp
@@ -2,14 +2,6 @@
 
 int main()
 {
-    //Game game(Player::Type::RandomPlayer, Player::Type::RandomPlayer);
-    //game.play(20);
-
-    ChessboardPrinter printer;
-    Chessboard board("k7/2K5/8/8/8/8/8/8");
-    std::vector<char> offsets;
-    board.getTeamOffsets(Team::Player2, offsets);
-    board.getTeamOffsets(Team::Player1, offsets);
-    for (const auto& i : offsets)
-        printer.printMoves(i, board);
+    Game game(Player::Type::RandomPlayer, Player::Type::RandomPlayer);
+    game.play(200);
 }
